fb9a43ca94ffef46d2897f89f704de89870e7bc1
David <Drodriguez819@users.noreply.github.com>
2025-11-11 02:43:32 -0600

    Update wsgi.py

--- Tow_Truck_Backend/wsgi.py
+++ Tow_Truck_Backend/wsgi.py
@@ -1,6 +1,9 @@
+import sys
+import os
+
+# Add the app directory to the path
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import create_app
 
 app = create_app()
-
-if __name__ == "__main__":
-    app.run()
